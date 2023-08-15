62cc17a74566b9cd63e41610f1b73a2fbb77d8f8
Vijay Reddy Dudipala <vijaydudipala88@gmail.com>
2023-08-15 15:31:06 -0500

    Exception included

--- src/main/java/com/springboot/customerapi/Service/CustomerServiceImpl.java
+++ src/main/java/com/springboot/customerapi/Service/CustomerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.springboot.customerapi.Service;
 
 import com.springboot.customerapi.entity.Customer;
+import com.springboot.customerapi.exception.CustomerNotFoundException;
 import com.springboot.customerapi.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
@@ -17,29 +18,35 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public String createCustomer(Customer customer) {
+        //More Business Logic
         customerRepository.save(customer);
         return "success";
     }
 
     @Override
     public String updateCustomer(Customer customer) {
+        //More Business Logic
         customerRepository.save(customer);
         return "success";
     }
 
     @Override
     public String deleteCustomer(long id) {
+        //More Business Logic
         customerRepository.deleteById(id);
         return "success";
     }
 
     @Override
     public Customer getCustomer(long id) {
+        if(customerRepository.findById(id).isEmpty())
+            throw new CustomerNotFoundException("Requested Customer does not exist");
          return customerRepository.findById(id).get();
     }
 
     @Override
     public List<Customer> getAllCustomers() {
+        //More Business Logic
         return customerRepository.findAll();
     }
 }
