db595dbddca2c21a5b02883331281008b73c678f
Vijay Reddy Dudipala <vijaydudipala88@gmail.com>
2023-08-16 14:41:26 -0500

    Custom Response Added

--- src/main/java/com/springboot/customerapi/controller/CustomerController.java
+++ src/main/java/com/springboot/customerapi/controller/CustomerController.java
@@ -1,7 +1,10 @@
 package com.springboot.customerapi.controller;
 
 import com.springboot.customerapi.Service.CustomerService;
+import com.springboot.customerapi.customresponse.ResponseHandler;
 import com.springboot.customerapi.entity.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +18,11 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
-
+    //Reads the Specific customer details
     @GetMapping("{id}")
-    public Customer getCreateCustomerDetails(@PathVariable("id") long id){
-        return  customerService.getCustomer(id);
+    public ResponseEntity<Object> getCreateCustomerDetails(@PathVariable("id") long id){
+      return  ResponseHandler.responseBuilder("Requested customer details are given here",
+                HttpStatus.OK,customerService.getCustomer(id));
     }
     @PostMapping
     public String createCustomerDetails(@RequestBody Customer customer){
@@ -35,6 +39,7 @@ public class CustomerController {
         customerService.deleteCustomer(id);
         return "Customer deleted successfully";
     }
+    //Reads All Customer details
     @GetMapping
     public List<Customer> getAllCustomerDetails(){
         return customerService.getAllCustomers();
